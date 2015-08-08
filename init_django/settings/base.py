2ef18ab159d886ef577d345b9f9fc2bc34e9e9a5
Kristoffer Dalby <kradalby@kradalby.no>
2015-08-08 11:20:32 +0200

    added logging as default option

--- init_django/settings/base.py
+++ init_django/settings/base.py
@@ -115,3 +115,36 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "..", "media")
 MEDIA_URL = '/media/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, '..', 'init_django.log'),
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['file'],
+            'propagate': True,
+            'level':'DEBUG',
+        },
+        'apps.sample_app': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+    }
+}
